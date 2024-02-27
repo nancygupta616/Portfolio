3220c8a3e3a6ac826e4cfa6834c10b5582d1472d
Nancy Gupta <nancysit616@gmail.com>
2024-02-27 20:57:25 +0530

    form-link-added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,7 +8,7 @@ const Contact = () => {
                     <h1 className='text-4xl text-red-600 font-extrabold border-b-4 border-red-600 p-2 inline z-10'>Contact</h1>
                 </div>
                 <div className='flex justify-center mb-20 mt-20'>
-                    <form action="https://getform.io/f/74275214-e22c-46a2-9959-fe534ed76c0d" method='POST' className='flex flex-col w-full max-w-xl'>
+                    <form action="https://getform.io/f/wdLLgjdj" method='POST' className='flex flex-col w-full max-w-xl'>
                         <input type="text" name="name" placeholder='Enter your name' className='p-2 bg-white focus:bg-white border-2 rounded-md text-black focus:outline-none mb-4 w-full' id="" />
                         <input type="email" name="email" placeholder='Enter your email' className='my-4 p-2 bg-white border-2 rounded-md text-black focus:bg-white focus:outline-none mb-4 w-full' id="" />
                         <textarea name="message" id="" placeholder='Enter your message...' className='p-2 bg-white border-2 rounded-md text-black focus:outline-none focus:bg-white mb-4 w-full' rows="10" cols={50}></textarea>
