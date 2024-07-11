bf3dd133b11a559ce9392ae8a4c26821a2254661
Nancy Gupta <nancysit616@gmail.com>
2024-07-11 13:45:21 +0530

    first commit

--- src/data.jsx
+++ src/data.jsx
@@ -1,4 +1,4 @@
-import project1 from './assets/projects/project1.jpeg';
+import project1 from './assets/projects/project1.png';
 import project2 from './assets/projects/project2.jpeg';
 import project3 from './assets/projects/project3.jpeg';
 import project4 from './assets/projects/project4.jpeg';
@@ -6,19 +6,19 @@ import project4 from './assets/projects/project4.jpeg';
 
 export const projects = [
     {
-      title: "Projects E-commerce Store",
-      subtitle: "React, Redux, HTML5, SCSS",
-      description: "Crafted an e-commerce platform with responsive and interactive UI, showcasing a wide array of product categories. Seamlessly integrated product listings, detailed views, and a dynamic shopping cart, leveraging fake APIs for an authentic experience.",
+      title: "E-commerce",
+      subtitle: "React, Express JS, Node JS, MongoDB",
+      description: "Built a dynamic interface with React JS for product displays, cart management, and user authentication. Created RESTful APIs for seamless CRUD operations and user login. Designed a robust admin panel for product addition, updates, and inventory management.",
       image: project1,
-      link: "https://react-e-commerce-nancy.netlify.app/",
-      codeLink: "https://github.com/nancygupta616/E-commerce-app"
+      link: "https://shopper-frontend-v2f0.onrender.com/",
+      codeLink: "https://github.com/nancygupta616/E-commerce/tree/main"
     },
     {
       title: "Netflix Clone",
       subtitle: "React, HTML5, SCSS",
       description: "Developed a Netflix clone web application using React.js, HTML5, CSS3, and JavaScript. Implemented responsive design principles to ensure optimal viewing experience across various devices and screen sizes.",
       image: project2,
-      link: "https://netflix789.netlify.app/",
+      link: "https://netflix-clone-hqkh.onrender.com/",
       codeLink: "https://github.com/nancygupta616/Netflix-clone",
     },
     {
